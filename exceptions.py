26ded775c50145fdf9b1ca42b442c2d4c57f68b1
CohleM <manish981194@gmail.com>
2025-06-08 22:34:16 +0545

    first push

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+class ToolError(Exception):
+    def __init__(self, message):
+        super.__init__(message)
+        self.message = message
+    
+    def __str__(self):
+        return self.message
+    
+
+
